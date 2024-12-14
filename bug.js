6d5306dc93158258a47366f926855363a88c84b4
BugHunter-X <askzenai@gmail.com>
2024-12-14 16:38:08 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This bug occurs when using the Expo Camera API with a custom component. The camera preview renders correctly, but taking a picture results in a blank image. This happens inconsistently, sometimes working and other times failing without any clear pattern or error message in the console.
